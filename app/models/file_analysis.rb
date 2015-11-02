fe147d7dbc3675c77b8e192d4116721f7a969770
German Escobar <german.escobarc@gmail.com>
2015-11-02 18:45:58 -0500

    Annotate models

--- app/models/file_analysis.rb
+++ app/models/file_analysis.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: file_analyses
+#
+#  id            :integer          not null, primary key
+#  filename      :string
+#  status        :integer          default(0)
+#  analyzed_rows :integer          default(0)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class FileAnalysis < ActiveRecord::Base
   enum status: [:pending, :processing, :finished]
 end
--- test/models/file_analysis_test.rb
+++ test/models/file_analysis_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: file_analyses
+#
+#  id            :integer          not null, primary key
+#  filename      :string
+#  status        :integer          default(0)
+#  analyzed_rows :integer          default(0)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'test_helper'
 
 class FileAnalysisTest < ActiveSupport::TestCase
