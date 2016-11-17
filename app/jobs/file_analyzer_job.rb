66c75f10abde3bd36e19f40fb33f626eaa161ce4
German Escobar <german.escobarc@gmail.com>
2016-11-17 18:38:32 -0500

    Refactor to use jobs

--- app/jobs/file_analyzer_job.rb
+++ app/jobs/file_analyzer_job.rb
@@ -9,8 +9,10 @@ class FileAnalyzerJob < ActiveJob::Base
     csv.each do |row|
       sleep 5 # simulate heavy processing
       file_analysis.update(analyzed_rows: file_analysis.analyzed_rows + 1)
+      Pusher.trigger("file-analysis-#{file_analysis.id}", 'update', file_analysis.as_json)
     end
-    
+
     file_analysis.update(status: :finished)
+    Pusher.trigger("file-analysis-#{file_analysis.id}", 'finished', {})
   end
 end
---
+++ config/initializers/pusher.rb
@@ -0,0 +1,4 @@
+PUSHER_KEY = "b5c9929b8e0248f2816e"
+
+Pusher.url = "https://#{PUSHER_KEY}:28538b1a8e721262156b@api.pusherapp.com/apps/158972"
+Pusher.logger = Rails.logger
